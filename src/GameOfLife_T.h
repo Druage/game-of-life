8fd8d2384ae2d1cc2573fc71145d91c84b78d617
Lee Lazarecky <leelazarecky@gmail.com>
2022-05-17 17:24:05 -0400

    Formatted via clangtidy

--- src/GameOfLife_T.h
+++ src/GameOfLife_T.h
@@ -1,172 +1,175 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
 #include <array>
 #include <functional>
+#include <iostream>
+#include <vector>
 
-template<int TRows, int TCols>
-class GameOfLife_T {
+template <int TRows, int TCols> class GameOfLife_T {
 
-    using CollectedCells = std::vector<std::pair<int, int>>;
-    int m_rows{TRows};
-    int m_cols{TCols};
+  enum Ops {
+    Underpopulation,
+    Overpopulation,
+    Reproduction,
+    All = Underpopulation & Overpopulation & Reproduction
+  };
+
+  using CollectedCells = std::vector<std::pair<int, int>>;
+  int m_rows{TRows};
+  int m_cols{TCols};
 
 public:
-    std::array<std::array<int, TRows>, TCols> gameGrid;
+  std::array<std::array<int, TRows>, TCols> gameGrid;
 
-    void seed(std::initializer_list<std::pair<int, int>> values);
+  void seed(std::initializer_list<std::pair<int, int>> values);
 
-    void tick();
+  void tick(Ops ops = Ops::All);
 
-    void print();
+  void print();
 
 public:
-    std::array<int, TRows> &operator[](int row);
+  std::array<int, TRows> &operator[](int row);
 
-    [[nodiscard]] int size() const;
+  [[nodiscard]] int size() const;
 
-    [[nodiscard]] int rows() const;
+  [[nodiscard]] int rows() const;
 
-    [[nodiscard]] int cols() const;
+  [[nodiscard]] int cols() const;
 
 private:
-    inline bool cellIsOverpopulated(int cellSum);
+  inline bool cellIsOverpopulated(int cellSum);
 
-    inline bool cellIsUnderpopulated(int cellSum);
+  inline bool cellIsUnderpopulated(int cellSum);
 
-    inline void setCellsSimultaneously(CollectedCells &cellsToChange, int value);
+  inline void setCellsSimultaneously(CollectedCells &cellsToChange, int value);
 
-    inline bool cellIsAtEdge(int row, int col);
+  inline bool cellIsAtEdge(int row, int col);
 
-    inline void
-    collectCellsIf(int row, int col, CollectedCells &destination, std::function<bool(int)> &&collectCondition);
+  inline void collectCellsIf(int row, int col, CollectedCells &destination,
+                             std::function<bool(int)> &&collectCondition);
 };
 
-template<int TWidth, int THeight>
-void GameOfLife_T<TWidth, THeight>::seed(std::initializer_list<std::pair<int, int>> values) {
-    for (const auto &coords: values) {
-        auto x = coords.first;
-        auto y = coords.second;
-        gameGrid[x][y] = 1;
-    }
+template <int TWidth, int THeight>
+void GameOfLife_T<TWidth, THeight>::seed(
+    std::initializer_list<std::pair<int, int>> values) {
+  for (const auto &coords : values) {
+    auto x = coords.first;
+    auto y = coords.second;
+    gameGrid[x][y] = 1;
+  }
 }
 
-template<int TWidth, int THeight>
-void GameOfLife_T<TWidth, THeight>::tick() {
+template <int TWidth, int THeight>
+void GameOfLife_T<TWidth, THeight>::tick(Ops ops) {
 
 #ifdef LOG_STATE
-    std::cout << "Before:" << std::endl;
-    print();
-    std::cout << std::endl;
+  std::cout << "Before:" << std::endl;
+  print();
+  std::cout << std::endl;
 #endif
 
-    CollectedCells cellsToKill;
-    CollectedCells cellsBorn;
+  CollectedCells cellsToKill;
+  CollectedCells cellsBorn;
 
-    for (int row = 0; row < TWidth; ++row) {
-        for (int col = 0; col < THeight; ++col) {
+  for (int row = 0; row < TWidth; ++row) {
+    for (int col = 0; col < THeight; ++col) {
 
-            if (gameGrid[row][col]) {
+      if (gameGrid[row][col]) {
 
-                if (cellIsAtEdge(row, col)) {
-                    cellsToKill.emplace_back(row, col);
-                } else {
-                    collectCellsIf(row, col, cellsToKill, [&](int cellSum) -> bool {
-                        return cellIsUnderpopulated(cellSum) || cellIsOverpopulated(cellSum);
-                    });
-                }
-
-            } else {
-                collectCellsIf(row, col, cellsBorn, [&](int cellSum) -> bool {
-                    return cellSum == 3;
-                });
-            }
-
+        if (cellIsAtEdge(row, col)) {
+          cellsToKill.emplace_back(row, col);
+        } else {
+          collectCellsIf(row, col, cellsToKill, [&](int cellSum) -> bool {
+            return cellIsUnderpopulated(cellSum) ||
+                   cellIsOverpopulated(cellSum);
+          });
         }
-    }
 
-    setCellsSimultaneously(cellsToKill, 0);
-    setCellsSimultaneously(cellsBorn, 1);
+      } else {
+        collectCellsIf(row, col, cellsBorn,
+                       [&](int cellSum) -> bool { return cellSum == 3; });
+      }
+    }
+  }
+
+  setCellsSimultaneously(cellsToKill, 0);
+  setCellsSimultaneously(cellsBorn, 1);
 
 #ifdef LOG_STATE
-    std::cout << "After:" << std::endl;
-    print();
-    std::cout << std::endl;
+  std::cout << "After:" << std::endl;
+  print();
+  std::cout << std::endl;
 #endif
 }
 
-template<int TWidth, int THeight>
-void GameOfLife_T<TWidth, THeight>::print() {
-    for (int w = 0; w < TWidth; ++w) {
-        for (int h = 0; h < THeight; ++h) {
-            std::cout << gameGrid[w][h] << " ";
-        }
-        std::cout << std::endl;
+template <int TWidth, int THeight> void GameOfLife_T<TWidth, THeight>::print() {
+  for (int w = 0; w < TWidth; ++w) {
+    for (int h = 0; h < THeight; ++h) {
+      std::cout << gameGrid[w][h] << " ";
     }
+    std::cout << std::endl;
+  }
 }
 
-template<int TWidth, int THeight>
+template <int TWidth, int THeight>
 bool GameOfLife_T<TWidth, THeight>::cellIsOverpopulated(int cellSum) {
-    return cellSum > 3;
+  return cellSum > 3;
 }
 
-template<int TWidth, int THeight>
+template <int TWidth, int THeight>
 bool GameOfLife_T<TWidth, THeight>::cellIsUnderpopulated(int cellSum) {
-    return cellSum < 2;
+  return cellSum < 2;
 }
 
-template<int TWidth, int THeight>
-void GameOfLife_T<TWidth, THeight>::setCellsSimultaneously(CollectedCells &cellsToChange, int value) {
-    for (auto cell: cellsToChange) {
-        gameGrid[cell.first][cell.second] = value;
-    }
+template <int TWidth, int THeight>
+void GameOfLife_T<TWidth, THeight>::setCellsSimultaneously(
+    CollectedCells &cellsToChange, int value) {
+  for (auto cell : cellsToChange) {
+    gameGrid[cell.first][cell.second] = value;
+  }
 }
 
-template<int TWidth, int THeight>
+template <int TWidth, int THeight>
 bool GameOfLife_T<TWidth, THeight>::cellIsAtEdge(int row, int col) {
-    return (row == 0 || col == 0 || row == TWidth - 1 || col == THeight - 1);
+  return (row == 0 || col == 0 || row == TWidth - 1 || col == THeight - 1);
 }
 
-template<int TWidth, int THeight>
-void GameOfLife_T<TWidth, THeight>::collectCellsIf(int row, int col, GameOfLife_T::CollectedCells &destination,
-                                                   std::function<bool(int)> &&collectCondition) {
+template <int TWidth, int THeight>
+void GameOfLife_T<TWidth, THeight>::collectCellsIf(
+    int row, int col, GameOfLife_T::CollectedCells &destination,
+    std::function<bool(int)> &&collectCondition) {
 
-    const int rightCell = gameGrid[row + 1][col];
-    const int leftCell = gameGrid[row - 1][col];
-    const int topCell = gameGrid[row][col - 1];
-    const int bottomCell = gameGrid[row][col + 1];
+  const int rightCell = gameGrid[row + 1][col];
+  const int leftCell = gameGrid[row - 1][col];
+  const int topCell = gameGrid[row][col - 1];
+  const int bottomCell = gameGrid[row][col + 1];
 
-    const int topLeftCell = gameGrid[row - 1][col - 1];
-    const int topRightCell = gameGrid[row + 1][col - 1];
-    const int bottomLeftCell = gameGrid[row - 1][col + 1];
-    const int bottomRightCell = gameGrid[row + 1][col + 1];
+  const int topLeftCell = gameGrid[row - 1][col - 1];
+  const int topRightCell = gameGrid[row + 1][col - 1];
+  const int bottomLeftCell = gameGrid[row - 1][col + 1];
+  const int bottomRightCell = gameGrid[row + 1][col + 1];
 
-    auto cellSum = (rightCell + leftCell + topCell + bottomCell
-                    + topLeftCell + topRightCell + bottomLeftCell +
-                    bottomRightCell);
+  auto cellSum = (rightCell + leftCell + topCell + bottomCell + topLeftCell +
+                  topRightCell + bottomLeftCell + bottomRightCell);
 
-    if (collectCondition(cellSum)) {
-        destination.emplace_back(row, col);
-    }
+  if (collectCondition(cellSum)) {
+    destination.emplace_back(row, col);
+  }
 }
 
-template<int TRows, int TCols>
+template <int TRows, int TCols>
 std::array<int, TRows> &GameOfLife_T<TRows, TCols>::operator[](int row) {
-    return gameGrid[row];
+  return gameGrid[row];
 }
 
-template<int TRows, int TCols>
-int GameOfLife_T<TRows, TCols>::size() const {
-    return m_rows * m_cols;
+template <int TRows, int TCols> int GameOfLife_T<TRows, TCols>::size() const {
+  return m_rows * m_cols;
 }
 
-template<int TRows, int TCols>
-int GameOfLife_T<TRows, TCols>::rows() const {
-    return m_rows;
+template <int TRows, int TCols> int GameOfLife_T<TRows, TCols>::rows() const {
+  return m_rows;
 }
 
-template<int TRows, int TCols>
-int GameOfLife_T<TRows, TCols>::cols() const {
-    return m_cols;
+template <int TRows, int TCols> int GameOfLife_T<TRows, TCols>::cols() const {
+  return m_cols;
 }
--- src/test/GameOfLifeTest.cpp
+++ src/test/GameOfLifeTest.cpp
@@ -1,8 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #define LOG_STATE
 
-#include "catch.hpp"
 #include "../GameOfLife_T.h"
+#include "catch.hpp"
 #include "utils/utils.h"
 
 using namespace test;
@@ -13,295 +13,284 @@ const int HEIGHT = 5;
 using GameOfLife = GameOfLife_T<WIDTH, HEIGHT>;
 
 TEST_CASE("The Game Of Life", "[Rules]") {
-    GameOfLife gameOfLife{};
+  GameOfLife gameOfLife{};
 
-/*
- * 0 | 0 0 0 0 0
- * 1 | 0 0 0 0 0
- * 2 | 0 0 0 0 0
- * 3 | 0 0 0 0 0
- * 4 | 0 0 0 0 0
- *   -----------
- *     0 1 2 3 4
- */
+  /*
+   * 0 | 0 0 0 0 0
+   * 1 | 0 0 0 0 0
+   * 2 | 0 0 0 0 0
+   * 3 | 0 0 0 0 0
+   * 4 | 0 0 0 0 0
+   *   -----------
+   *     0 1 2 3 4
+   */
 
-    SECTION("The grid is the correct size and initialized to 0") {
-        REQUIRE(gameOfLife.rows() == WIDTH);
-        REQUIRE(gameOfLife.cols() == HEIGHT);
-        REQUIRE(gameOfLife.size() == WIDTH * HEIGHT);
-        REQUIRE(containsAll(gameOfLife, 0));
-    }
+  SECTION("The grid is the correct size and initialized to 0") {
+    REQUIRE(gameOfLife.rows() == WIDTH);
+    REQUIRE(gameOfLife.cols() == HEIGHT);
+    REQUIRE(gameOfLife.size() == WIDTH * HEIGHT);
+    REQUIRE(containsAll(gameOfLife, 0));
+  }
 
-/*
- * 0 | 0 0 0 0 0
- * 1 | 0 1 1 0 0
- * 2 | 0 0 0 0 0
- * 3 | 0 0 0 0 0
- * 4 | 0 0 0 0 0
- *   -----------
- *     0 1 2 3 4
- */
+  /*
+   * 0 | 0 0 0 0 0
+   * 1 | 0 1 1 0 0
+   * 2 | 0 0 0 0 0
+   * 3 | 0 0 0 0 0
+   * 4 | 0 0 0 0 0
+   *   -----------
+   *     0 1 2 3 4
+   */
 
-    SECTION("The grid can be seeded") {
+  SECTION("The grid can be seeded") {
 
-        gameOfLife.seed({
-                                {1, 1},
-                                {2, 1}
-                        });
+    gameOfLife.seed({{1, 1}, {2, 1}});
 
-        REQUIRE(gameOfLife[1][1] == 1);
-        REQUIRE(gameOfLife[2][1] == 1);
+    REQUIRE(gameOfLife[1][1] == 1);
+    REQUIRE(gameOfLife[2][1] == 1);
 
-        REQUIRE(containsOnly(gameOfLife, {{1, 1},
-                                          {2, 1}}));
-    }
+    REQUIRE(containsOnly(gameOfLife, {{1, 1}, {2, 1}}));
+  }
 
-/*
- * 0 | 0 0 0 0 0
- * 1 | 0 1 1 0 0
- * 2 | 0 0 0 0 0
- * 3 | 0 0 0 0 0
- * 4 | 0 0 0 0 0
- *   -----------
- *     0 1 2 3 4
- */
+  /*
+   * 0 | 0 0 0 0 0
+   * 1 | 0 1 1 0 0
+   * 2 | 0 0 0 0 0
+   * 3 | 0 0 0 0 0
+   * 4 | 0 0 0 0 0
+   *   -----------
+   *     0 1 2 3 4
+   */
 
-    SECTION("seeding only 2 adjacent cells will die", "[Underpopulation]") {
-/*
- * 0 | 0 0 0 0 0
- * 1 | 0 1 1 0 0
- * 2 | 0 0 0 0 0
- * 3 | 0 0 0 0 0
- * 4 | 0 0 0 0 0
- *   -----------
- *     0 1 2 3 4
- */
-        gameOfLife.seed({
-                                {1, 1},
-                                {2, 1}
-                        });
-        gameOfLife.tick();
-        REQUIRE(containsAll(gameOfLife, 0));
-    }
+  SECTION("seeding only 2 adjacent cells will die", "[Underpopulation]") {
+    /*
+     * 0 | 0 0 0 0 0
+     * 1 | 0 1 1 0 0
+     * 2 | 0 0 0 0 0
+     * 3 | 0 0 0 0 0
+     * 4 | 0 0 0 0 0
+     *   -----------
+     *     0 1 2 3 4
+     */
+    gameOfLife.seed({{1, 1}, {2, 1}});
+    gameOfLife.tick();
+    REQUIRE(containsAll(gameOfLife, 0));
+  }
 
-    SECTION("seeding 3 adjacent will live, 1 will die", "[Underpopulation & Live]") {
-/*
- *   0 | 0 0 0 0 0
- *   1 | 0 1 0 0 0
- *   2 | 0 0 1 1 0
- *   3 | 0 0 1 0 0
- *   4 | 0 0 0 0 0
- *     -----------
- *       0 1 2 3 4
- */
+  SECTION("seeding 3 adjacent will live, 1 will die",
+          "[Underpopulation & Live]") {
+    /*
+     *   0 | 0 0 0 0 0
+     *   1 | 0 1 0 0 0
+     *   2 | 0 0 1 1 0
+     *   3 | 0 0 1 0 0
+     *   4 | 0 0 0 0 0
+     *     -----------
+     *       0 1 2 3 4
+     */
 
-        gameOfLife.seed({
-                                {1, 1},
-                                {2, 2},
-                                {3, 2},
-                                {2, 3},
-                        });
+    gameOfLife.seed({
+        {1, 1},
+        {2, 2},
+        {3, 2},
+        {2, 3},
+    });
 
-        gameOfLife.tick();
-        REQUIRE(containsOnly(gameOfLife, {
-                {2, 1},
+    gameOfLife.tick();
+    REQUIRE(containsOnly(gameOfLife, {
+                                         {2, 1},
 
-                {1, 2},
-                {2, 2},
-                {3, 2},
+                                         {1, 2},
+                                         {2, 2},
+                                         {3, 2},
 
-                {2, 3},
-                {3, 3},
-        }));
-    }
+                                         {2, 3},
+                                         {3, 3},
+                                     }));
+  }
 
-        /*
+  /*
+   *   0 | 0 0 0 0 0
+   *   1 | 0 1 1 0 0
+   *   2 | 0 1 1 1 0
+   *   3 | 0 0 1 1 0
+   *   4 | 0 0 0 0 0
+   *     -----------
+   *       0 1 2 3 4
+   */
+
+  SECTION("seeding 3 adjacent cells will live", "[Live]") {
+    /*
+     *   0 | 0 0 0 0 0
+     *   1 | 0 1 1 0 0
+     *   2 | 0 1 0 0 0
+     *   3 | 0 0 0 1 0
+     *   4 | 0 0 0 0 0
+     *     -----------
+     *       0 1 2 3 4
+     */
+
+    gameOfLife.seed({
+        {1, 1},
+        {2, 1},
+        {1, 2},
+    });
+
+    gameOfLife.tick();
+    REQUIRE(containsOnly(gameOfLife, {
+                                         {1, 1},
+                                         {2, 1},
+                                         {1, 2},
+                                         {2, 2},
+                                     }));
+  }
+
+  /*
+   *   0 | 0 0 0 0 0
+   *   1 | 0 1 1 0 0
+   *   2 | 0 1 1 0 0
+   *   3 | 0 0 0 0 0
+   *   4 | 0 0 0 0 0
+   *     -----------
+   *       0 1 2 3 4
+   */
+
+  SECTION("seeding 4 diagonally adjacent cells will live", "[Live]") {
+    /*
+     *   0 | 0 0 0 0 0
+     *   1 | 0 0 1 0 0
+     *   2 | 0 1 0 1 0
+     *   3 | 0 0 1 0 0
+     *   4 | 0 0 0 0 0
+     *     -----------
+     *       0 1 2 3 4
+     */
+
+    gameOfLife.seed({
+        {2, 1},
+        {1, 2},
+        {3, 2},
+        {2, 3},
+    });
+
+    gameOfLife.tick();
+    REQUIRE(containsOnly(gameOfLife, {
+                                         {2, 1},
+                                         {1, 2},
+                                         {3, 2},
+                                         {2, 3},
+                                     }));
+  }
+
+  SECTION("seeding 5 diagonally & horizontally adjacent cells will live",
+          "[Live]") {
+    /*
+     *   0 | 0 0 0 0 0
+     *   1 | 0 1 1 0 0
+     *   2 | 0 1 0 1 0
+     *   3 | 0 0 1 0 0
+     *   4 | 0 0 0 0 0
+     *     -----------
+     *       0 1 2 3 4
+     */
+
+    gameOfLife.seed({
+        {1, 1},
+        {2, 1},
+        {1, 2},
+        {3, 2},
+        {2, 3},
+    });
+
+    gameOfLife.tick();
+    REQUIRE(containsOnly(gameOfLife, {
+                                         {1, 1},
+                                         {2, 1},
+                                         {1, 2},
+                                         {3, 2},
+                                         {2, 3},
+                                     }));
+  }
+
+  SECTION("Overpopulation causes 3 cells to die, the remaining 3 live and "
+          "reproduce",
+          "[Overpopulation & Live & Reproduction]") {
+    /*
      *   0 | 0 0 0 0 0
      *   1 | 0 1 1 0 0
      *   2 | 0 1 1 1 0
-     *   3 | 0 0 1 1 0
+     *   3 | 0 0 1 0 0
      *   4 | 0 0 0 0 0
      *     -----------
      *       0 1 2 3 4
      */
 
+    gameOfLife.seed({
+        {1, 1},
+        {2, 1},
+        {1, 2},
+        {2, 2},
+        {3, 2},
+        {2, 3},
+    });
 
-    SECTION("seeding 3 adjacent cells will live", "[Live]") {
-/*
- *   0 | 0 0 0 0 0
- *   1 | 0 1 1 0 0
- *   2 | 0 1 0 0 0
- *   3 | 0 0 0 1 0
- *   4 | 0 0 0 0 0
- *     -----------
- *       0 1 2 3 4
- */
+    gameOfLife.tick();
+    REQUIRE(containsOnly(gameOfLife, {
+                                         {1, 1},
+                                         {3, 1},
+                                         {3, 2},
+                                         {1, 3},
+                                         {2, 3},
+                                         {3, 3},
+                                     }));
 
-        gameOfLife.seed({
-                                {1, 1},
-                                {2, 1},
-                                {1, 2},
-                        });
-
-        gameOfLife.tick();
-        REQUIRE(containsOnly(gameOfLife, {
-                {1, 1},
-                {2, 1},
-                {1, 2},
-                {2, 2},
-        }));
-    }
-
-        /*
+    /*
      *   0 | 0 0 0 0 0
-     *   1 | 0 1 1 0 0
-     *   2 | 0 1 1 0 0
-     *   3 | 0 0 0 0 0
+     *   1 | 0 1 0 1 0
+     *   2 | 0 0 0 1 0
+     *   3 | 0 1 1 1 0
      *   4 | 0 0 0 0 0
      *     -----------
      *       0 1 2 3 4
      */
+  }
 
-    SECTION("seeding 4 diagonally adjacent cells will live", "[Live]") {
-/*
- *   0 | 0 0 0 0 0
- *   1 | 0 0 1 0 0
- *   2 | 0 1 0 1 0
- *   3 | 0 0 1 0 0
- *   4 | 0 0 0 0 0
- *     -----------
- *       0 1 2 3 4
- */
+  SECTION("cells will die if they reach the end of the grid",
+          "[Bounds Check]") {
+    /*
+     *   0 | 1 1 0 0 0
+     *   1 | 1 0 0 0 0
+     *   2 | 0 0 0 0 0
+     *   3 | 0 0 0 0 1
+     *   4 | 0 0 0 1 1
+     *     -----------
+     *       0 1 2 3 4
+     */
 
-        gameOfLife.seed({
-                                {2, 1},
-                                {1, 2},
-                                {3, 2},
-                                {2, 3},
-                        });
+    gameOfLife.seed({
+        {0, 0},
+        {1, 0},
+        {0, 1},
 
-        gameOfLife.tick();
-        REQUIRE(containsOnly(gameOfLife, {
-                {2, 1},
-                {1, 2},
-                {3, 2},
-                {2, 3},
-        }));
+        {4, 3},
+        {3, 4},
+        {4, 4},
+    });
 
-    }
+    gameOfLife.tick();
+    REQUIRE(containsOnly(gameOfLife, {
+                                         {1, 1},
+                                         {3, 3},
+                                     }));
 
-    SECTION("seeding 5 diagonally & horizontally adjacent cells will live", "[Live]") {
-/*
- *   0 | 0 0 0 0 0
- *   1 | 0 1 1 0 0
- *   2 | 0 1 0 1 0
- *   3 | 0 0 1 0 0
- *   4 | 0 0 0 0 0
- *     -----------
- *       0 1 2 3 4
- */
-
-        gameOfLife.seed({
-                                {1, 1},
-                                {2, 1},
-                                {1, 2},
-                                {3, 2},
-                                {2, 3},
-                        });
-
-        gameOfLife.tick();
-        REQUIRE(containsOnly(gameOfLife, {
-                {1, 1},
-                {2, 1},
-                {1, 2},
-                {3, 2},
-                {2, 3},
-        }));
-
-    }
-
-    SECTION("Overpopulation causes 3 cells to die, the remaining 3 live and reproduce",
-            "[Overpopulation & Live & Reproduction]") {
-/*
- *   0 | 0 0 0 0 0
- *   1 | 0 1 1 0 0
- *   2 | 0 1 1 1 0
- *   3 | 0 0 1 0 0
- *   4 | 0 0 0 0 0
- *     -----------
- *       0 1 2 3 4
- */
-
-
-        gameOfLife.seed({
-                                {1, 1},
-                                {2, 1},
-                                {1, 2},
-                                {2, 2},
-                                {3, 2},
-                                {2, 3},
-                        });
-
-        gameOfLife.tick();
-        REQUIRE(containsOnly(gameOfLife, {
-                {1, 1},
-                {3, 1},
-                {3, 2},
-                {1, 3},
-                {2, 3},
-                {3, 3},
-        }));
-
-        /*
- *   0 | 0 0 0 0 0
- *   1 | 0 1 0 1 0
- *   2 | 0 0 0 1 0
- *   3 | 0 1 1 1 0
- *   4 | 0 0 0 0 0
- *     -----------
- *       0 1 2 3 4
- */
-
-    }
-
-    SECTION("cells will die if they reach the end of the grid", "[Bounds Check]") {
-/*
- *   0 | 1 1 0 0 0
- *   1 | 1 0 0 0 0
- *   2 | 0 0 0 0 0
- *   3 | 0 0 0 0 1
- *   4 | 0 0 0 1 1
- *     -----------
- *       0 1 2 3 4
- */
-
-        gameOfLife.seed({
-                                {0, 0},
-                                {1, 0},
-                                {0, 1},
-
-                                {4, 3},
-                                {3, 4},
-                                {4, 4},
-                        });
-
-        gameOfLife.tick();
-        REQUIRE(containsOnly(gameOfLife, {
-                {1, 1},
-                {3, 3},
-        }));
-
-/*
- *   0 | 0 0 0 0 0
- *   1 | 0 1 0 0 0
- *   2 | 0 0 0 0 0
- *   3 | 0 0 0 1 0
- *   4 | 0 0 0 0 0
- *     -----------
- *       0 1 2 3 4
- */
-
-
-    }
-
+    /*
+     *   0 | 0 0 0 0 0
+     *   1 | 0 1 0 0 0
+     *   2 | 0 0 0 0 0
+     *   3 | 0 0 0 1 0
+     *   4 | 0 0 0 0 0
+     *     -----------
+     *       0 1 2 3 4
+     */
+  }
 }
