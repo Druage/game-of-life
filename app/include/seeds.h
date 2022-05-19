4029c4981dfa1b1a25ad074761a675d08b96ae18
Lee Lazarecky <leelazarecky@gmail.com>
2022-05-18 22:02:12 -0400

    Adds two seeds to pick from

--- app/include/seeds.h
+++ app/include/seeds.h
@@ -2,6 +2,17 @@
 
 #include <utility>
 
+const std::initializer_list<std::pair<int, int>> SIMPLE_GLIDER_SEED = {
+        {2, 1},
+
+        {3, 2},
+
+        {1, 3},
+        {2, 3},
+        {3, 3},
+};
+
+
 const std::initializer_list<std::pair<int, int>> INTERESTING_SEED = {
         {2, 1},
 
@@ -26,5 +37,29 @@ const std::initializer_list<std::pair<int, int>> INTERESTING_SEED = {
         {11, 13},
         {12, 13},
         {13, 13},
+
+        {92, 91},
+
+        {93, 92},
+
+        {91, 3},
+        {92, 93},
+        {93, 93},
+
+        {82, 81},
+
+        {85, 87},
+
+        {81, 83},
+        {82, 83},
+        {83, 83},
+
+        {72, 71},
+
+        {70, 72},
+
+        {71, 73},
+        {72, 73},
+        {73, 73},
 
 };
